066db395db0b8c5ec224326059b6b8d6678afd37
Nick Pettazzoni <pettazz@gmail.com>
2012-02-24 03:12:06 -0500

    I am not good with computer.

--- tvrobot.py
+++ tvrobot.py
@@ -54,7 +54,7 @@ class TvRobot:
                 try:
                     self.driver = webdriver.Remote("http://%s:%s/wd/hub"%
                         (config.SELENIUM['server'], config.SELENIUM['port']),
-                        webdriver.DesiredCapabilities.FIREFOX)
+                        webdriver.DesiredCapabilities.HTMLUNIT)
                     #self.driver = webdriver.Firefox()
                     break
                 except:
