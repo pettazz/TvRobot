31ccde9a435bcaaefdc14af3356a1019a730e000
Nick Pettazzoni <pettazz@gmail.com>
2012-11-16 00:14:58 -0500

    i dont know why lock manager had anything to do with user ids in the first place

--- core/lock_manager.py
+++ core/lock_manager.py
@@ -17,49 +17,43 @@ class LockManager:
         result = DatabaseManager().fetchone_query_and_close(query, {'lock_type': lock_type, 'now': now})
         return result is not None
     
-    def set_lock(self, lock_type, user_id=None):
+    def set_lock(self, lock_type):
         if self.is_locked(lock_type):
             raise Exception("Could not set %s lock, already locked." % lock_type)
         guid = uuid.uuid4()
         now = int(time.time() * 1000)
-        if user_id is None:
-            user_id = UserManager().get_user_id()
         query = """
             INSERT INTO TaskLock
-            (guid, type, User, time_locked)
+            (guid, type, time_locked)
             VALUES 
-            (%(guid)s, %(lock_type)s, %(userid)s, %(now)s)
+            (%(guid)s, %(lock_type)s, %(now)s)
         """
-        result = DatabaseManager().execute_query_and_close(query, {'guid': guid, 'lock_type': lock_type, 'userid': user_id, 'now': now})
+        result = DatabaseManager().execute_query_and_close(query, {'guid': guid, 'lock_type': lock_type, 'now': now})
         if result:
             return guid
         else:
             raise Exception("Could not set %s lock." % lock_type)
     
-    def unlock(self, lock_guid=None, user_id=None, lock_type=None):
+    def unlock(self, lock_guid=None, lock_type=None):
         now = int(time.time() * 1000)
         if lock_guid is None:
-            if user_id is None:
-                user_id = UserManager().get_user_id()
             if lock_type is None:
                 query = """
                     UPDATE TaskLock 
                     SET time_unlocked = %(now)s
                     WHERE
-                    User = %(user_id)s AND 
                     time_unlocked = 0
                 """
-                result = DatabaseManager().execute_query_and_close(query, {'now': now, 'user_id': user_id})
+                result = DatabaseManager().execute_query_and_close(query, {'now': now})
             else:
                 query = """
                     UPDATE TaskLock 
                     SET time_unlocked = %(now)s
                     WHERE
                     lock_type = %(lock_type)s AND
-                    User = %(user_id)s AND 
                     time_unlocked = 0
                 """
-                result = DatabaseManager().execute_query_and_close(query, {'lock_type': lock_type, 'now': now, 'user_id': user_id})
+                result = DatabaseManager().execute_query_and_close(query, {'lock_type': lock_type, 'now': now})
         else:
             query = """
                 UPDATE TaskLock 
