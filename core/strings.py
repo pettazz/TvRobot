22e6a3bb6114ba33f452856595bcfff081ef47f7
Nick Pettazzoni <pettazz@gmail.com>
2013-03-27 00:53:43 -0400

    every string that supports interpolation should be in unicode

--- core/strings.py
+++ core/strings.py
@@ -57,17 +57,17 @@ KILL_CAUGHT = "But I was just getting started. Okay fine, I'll stop."
 
 CAUGHT_EXCEPTION = "BEEEEEEEEEEEEEEEEEEEP. OW."
 
-FINDING_VIDEO_FILE = "beep booping torrent #%s"
-MOVING_VIDEO_FILE = "beep beep bopping %s file `%s`..." 
+FINDING_VIDEO_FILE = u"beep booping torrent #%s"
+MOVING_VIDEO_FILE = u"beep beep bopping %s file `%s`..." 
 
-UNRECOGNIZED_TORRENT = "Booeep. Do I know you? Skipping torrent # %s"
-TORRENT_DOWNLOADING = "Boop. This one is still working. Skipping torrent # %s"
-UNSUPPORTED_DOWNLOAD_TYPE = "Beeeeeeeooooppppp I can't do these kinds of downloads yet. :( Skipping torrent # %s"
-UNSUPPORTED_FILE_TYPE = "I don't know how to beep boop this kind of download yet. Skipping torrent # %s"
+UNRECOGNIZED_TORRENT = u"Booeep. Do I know you? Skipping torrent # %s"
+TORRENT_DOWNLOADING = u"Boop. This one is still working. Skipping torrent # %s"
+UNSUPPORTED_DOWNLOAD_TYPE = u"Beeeeeeeooooppppp I can't do these kinds of downloads yet. :( Skipping torrent # %s"
+UNSUPPORTED_FILE_TYPE = u"I don't know how to beep boop this kind of download yet. Skipping torrent # %s"
 
 DOWNLOAD_CLEAN_COMPLETED = "beep. File's done."
 
 ADDING_TORRENT = "beep beep. Starting to download this torrent."
 ADDING_MAGNET = "bzzzzzeeeeep Adding this magnet before it kills me."
-ADDING_DOWNLOAD = "beeboop. Remembering to clean this %s download up later."
+ADDING_DOWNLOAD = u"beeboop. Remembering to clean this %s download up later."
 ADD_COMPLETED = "Beeped the new torrent."
